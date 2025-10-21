5da1bf140d4f401af6452b3ba5ec7bda345254fa
Utkarsh Sinha <sinhautkarsh22@gmail.com>
2025-10-21 20:40:09 +0530

    Add script to calculate sum of numbers from 1 to 50

---
+++ Task2.py
@@ -0,0 +1,9 @@
+# Initialize a variable to store the sum
+total_sum = 0
+
+# Use a for loop to iterate from 1 to 50
+for i in range(1, 51):
+    total_sum += i  # Add each number to the total
+
+# Display the result
+print("The sum of numbers from 1 to 50:", total_sum)
