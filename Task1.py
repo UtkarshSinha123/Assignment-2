0056a71ac8f1388e2fc6407e2935bb34ec360aff
Utkarsh Sinha <sinhautkarsh22@gmail.com>
2025-10-21 20:38:10 +0530

    Add even or odd number checker

---
+++ Task1.py
@@ -0,0 +1,8 @@
+# Take an integer input from the user
+number = int(input("Enter a number: "))
+
+# Check whether the number is even or odd
+if number % 2 == 0:
+    print(number, "is an even number.")
+else:
+    print(number, "is an odd number.")
